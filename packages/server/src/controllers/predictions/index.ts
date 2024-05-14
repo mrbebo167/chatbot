9d180e0877e99d652f5cab1e0831286b1a542c65
hussain <hussain2017@outlook.com>
2024-05-14 21:01:13 +0300

    test3

--- packages/server/src/controllers/predictions/index.ts
+++ packages/server/src/controllers/predictions/index.ts
@@ -1,71 +1,70 @@
-import { Request, Response, NextFunction } from 'express'
-import { getRateLimiter } from '../../utils/rateLimit'
-import chatflowsService from '../../services/chatflows'
-import logger from '../../utils/logger'
-import predictionsServices from '../../services/predictions'
-import { InternalFlowiseError } from '../../errors/internalFlowiseError'
-import { StatusCodes } from 'http-status-codes'
+import { Request, Response, NextFunction } from 'express';
+import { getRateLimiter } from '../../utils/rateLimit';
+import chatflowsService from '../../services/chatflows';
+import logger from '../../utils/logger';
+import predictionsServices from '../../services/predictions';
+import { InternalFlowiseError } from '../../errors/internalFlowiseError';
+import { StatusCodes } from 'http-status-codes';
 
 // Send input message and get prediction result (External)
 const createPrediction = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        if (typeof req.params === 'undefined' || !req.params.id) {
+        if (!req.params.id) {
             throw new InternalFlowiseError(
                 StatusCodes.PRECONDITION_FAILED,
-                `Error: predictionsController.createPrediction - id not provided!`
-            )
+                'Error: predictionsController.createPrediction - id not provided!'
+            );
         }
+
         if (!req.body) {
             throw new InternalFlowiseError(
                 StatusCodes.PRECONDITION_FAILED,
-                `Error: predictionsController.createPrediction - body not provided!`
-            )
+                'Error: predictionsController.createPrediction - body not provided!'
+            );
         }
-        const chatflow = await chatflowsService.getChatflowById(req.params.id)
+
+        const chatflow = await chatflowsService.getChatflowById(req.params.id);
         if (!chatflow) {
-            throw new InternalFlowiseError(StatusCodes.NOT_FOUND, `Chatflow ${req.params.id} not found`)
+            throw new InternalFlowiseError(StatusCodes.NOT_FOUND, `Chatflow ${req.params.id} not found`);
         }
-        let isDomainAllowed = true
-        logger.info(`[server]: Request originated from ${req.headers.origin}`)
-        if (chatflow.chatbotConfig) {
-            const parsedConfig = JSON.parse(chatflow.chatbotConfig)
-            // check whether the first one is not empty. if it is empty that means the user set a value and then removed it.
-            const isValidAllowedOrigins = parsedConfig.allowedOrigins?.length && parsedConfig.allowedOrigins[0] !== ''
-            if (isValidAllowedOrigins) {
-                const originHeader = req.headers.origin as string
-                const origin = new URL(originHeader).host
-                isDomainAllowed =
-                    parsedConfig.allowedOrigins.filter((domain: string) => {
-                        try {
-                            const allowedOrigin = new URL(domain).host
-                            return origin === allowedOrigin
-                        } catch (e) {
-                            return false
-                        }
-                    }).length > 0
-            }
+
+        let isDomainAllowed = true;
+        logger.info(`[server]: Request originated from ${req.headers.origin}`);
+
+        if (chatflow.chatbotConfig && req.headers.origin) {
+            const parsedConfig = JSON.parse(chatflow.chatbotConfig);
+            const originHeader = new URL(req.headers.origin).host;
+
+            isDomainAllowed = parsedConfig.allowedOrigins.some(domain => {
+                try {
+                    return new URL(domain).host === originHeader;
+                } catch (e) {
+                    logger.error('Invalid domain URL:', domain);
+                    return false;
+                }
+            });
         }
+
         if (isDomainAllowed) {
-            //@ts-ignore
-            const apiResponse = await predictionsServices.buildChatflow(req, req?.io)
-            return res.json(apiResponse)
+            const apiResponse = await predictionsServices.buildChatflow(req, req?.io); // Assuming buildChatflow is corrected
+            return res.json(apiResponse);
         } else {
-            throw new InternalFlowiseError(StatusCodes.UNAUTHORIZED, `This site is not allowed to access this chatbot`)
+            throw new InternalFlowiseError(StatusCodes.UNAUTHORIZED, 'This site is not allowed to access this chatbot');
         }
     } catch (error) {
-        next(error)
+        next(error);
     }
-}
+};
 
 const getRateLimiterMiddleware = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        return getRateLimiter(req, res, next)
+        return getRateLimiter(req, res, next);
     } catch (error) {
-        next(error)
+        next(error);
     }
-}
+};
 
 export default {
     createPrediction,
     getRateLimiterMiddleware
-}
+};
